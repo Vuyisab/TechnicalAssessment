e03e6a6dd3427013e368ecede7827377039da028
Vuyisab <vbazaa3@gmail.com>
2024-03-17 11:43:55 +0200

    Finished task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+static string FindMajorityElement(int[] arr)
+{
+    int candidate = arr[0];
+    int count = 1;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (count == 0)
+        {
+            candidate = arr[i];
+            count = 1;
+        }
+        else if (arr[i] == candidate)
+        {
+            count++;
+        }
+        else
+        {
+            count--;
+        }
+    }
+
+    count = 0;
+    foreach (int num in arr)
+    {
+        if (num == candidate) count++;
+    }
+
+    if (count > arr.Length / 2) return candidate.ToString();
+    else return "No majority element";
+}
+
+int[] arr = { 3, 3, 4, 2, 4, 2, 2 };
+Console.WriteLine(FindMajorityElement(arr));
